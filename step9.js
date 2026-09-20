00fdcc54afed791db4174d1facf53636b27da2fe
MahdiCh18 <mahdicharafi12@gmail.com>
2026-09-20 15:08:53 +0100

    revise

--- step9.js
+++ step9.js
@@ -1,9 +1,9 @@
 // step9
-const nums4 = [1, 2, 3, 4]
-let temp1 = nums4.pop()
-let temp2 = nums4.shift()
+const nums = [1, 2, 3, 4]
+let temp1 = nums.pop()
+let temp2 = nums.shift()
 
-nums4.unshift(temp1)
-nums4.push(temp2)
+nums.unshift(temp1)
+nums.push(temp2)
 
-console.log(nums4)
+console.log(nums)
