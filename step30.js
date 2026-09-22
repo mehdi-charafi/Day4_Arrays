1d64c7e6e5dddf887ad95d99e05f1774e7cbda0d
Mehdi Charafi <mahdicharafi12@gmail.com>
2026-09-22 15:53:55 +0100

    Add files via upload

--- step30.js
+++ step30.js
@@ -2,8 +2,11 @@ let nums = [20, 13, 5, 43, 9]
 
 function contains(arr, value) {
    for (let i of arr) {
-    if (i == value) return true
-   }
+    if (i == value) {
+    return true
+}
+}
+return false
 }
 
-console.log(contains(nums, 5))
+console.log(contains(nums, 9)) 
